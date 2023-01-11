b54ff9526a38b295d17ce70292025c80ecf4733e
RyzenMu <muruga13in2022@gmail.com>
2023-01-11 21:06:33 +0530

    initialized bgColorChanger3

--- main.js
+++ main.js
@@ -1,15 +1,5 @@
 
 
-// print selected color on console 
-
-function select(value) {
-    console.log(value);
-    let body = document.getElementsByTagName('body');
-    console.log(body[0]);
-    body[0].style.backgroundColor = value;
-}
-
-// print selected color on html page
 
 
 
