3d0dd3c20c5a365286fb6bbdc95a8222bc98c3da
RyzenMu <muruga13in2022@gmail.com>
2023-01-11 21:27:27 +0530

    bgcc3 completed

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 
 
+let colors = ['red', 'blue', 'grey', 'green', 'yellow', 'black', 'orange', 'white', 'brown', 'violet'];
 
+
+button = document.querySelector("button");
+
+button.addEventListener("click", function (){
+    r = Math.floor(Math.random()*colors.length)
+    console.log(r);
+    var i = colors[r];
+    document.body.style.background = i;
+});
 
 
 
